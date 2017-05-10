d62aa8fa77c89e27f69a3352432056cbbe0562c7
Holly <hnguyen0711@gmail.com>
2017-05-09 21:54:41 -0700

    switched to c#, added pickups, score count, win text

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System;
+using UnityEngine.UI; // required to use "Text" object and other UI elements
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 800.0f;
+	public Text scoreText; // "scoreText" will store our UI Text object
+	private int count = 0; // "count" will keep track of how many cubes we picked up
+	public Text winText;
+
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		GetComponent<Rigidbody>().AddForce (movement * speed * Time.deltaTime);
+	}
+
+	void OnTriggerEnter(Collider other) { // when the ball enters a Trigger Collider
+		if (other.gameObject.tag == "PickUp") { // if the object that was entered has the tag "PickUp"
+			other.gameObject.SetActive (false); // then set that object to be inactive (hide it)
+			count += 1; // adds 1 to count each time we pick up a cube
+			scoreText.text = "Score: " + count; // updates the text property of scoreText
+		}
+		if (count >=16) {
+			winText.gameObject.SetActive(true);
+		}
+	}
+}
